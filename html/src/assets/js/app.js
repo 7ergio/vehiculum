9eab7bb56dd33d277674499b27351ab713b744bf
Sergio <7ergio@gmail.com>
2018-10-07 16:18:47 +0300

    finish

--- html/src/assets/js/app.js
+++ html/src/assets/js/app.js
@@ -10,6 +10,31 @@ jQuery($ => {
            $('.header__dropdown').slideToggle(500);
         });
 
+        // Car slider
+        $('.cars-slider-js').slick({
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 2,
+                        arrows: false,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 1,
+                        arrows: false,
+                        dots: true
+                    }
+                }
+            ]
+        });
+
     });
 
 });
